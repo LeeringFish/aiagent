43716f2e45551b29d0a986631944364889203b7f
LeeringFish <theleeringfish@proton.me>
2025-11-15 11:56:58 -0500

    refactor to accept prompt from command line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -7,12 +8,17 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    if len(sys.argv) != 2:
+        print('Error: No prompt provided. Example: uv run main.py "Explain binary search"')
+        sys.exit(1)
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    prompt = sys.argv[1]
 
     response = client.models.generate_content(model="gemini-2.0-flash-001", 
-                                              contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+                                              contents=prompt)
     
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
