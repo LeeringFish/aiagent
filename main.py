934f404594332cf032c6975e11a1759da5f940d4
LeeringFish <theleeringfish@proton.me>
2025-11-16 19:10:23 -0500

    add system_prompt and pass as system_instruction to model config

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
         sys.exit(1)
     
     user_prompt = " ".join(args[1:])
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -26,7 +27,8 @@ def main():
     ]
 
     response = client.models.generate_content(model="gemini-2.0-flash-001", 
-                                              contents=messages,)
+                                              contents=messages,
+                                              config=types.GenerateContentConfig(system_instruction=system_prompt),)
     
     if verbose:
         print(f"User prompt: {response.text}")
